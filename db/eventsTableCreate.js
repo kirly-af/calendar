00cde10f4d3887ba066a9a2614b2c90a309cdc38
Alexandre Fulgoni <a.fulgon@gmail.com>
2016-04-23 19:15:42 +0100

    add aws middleware

---
+++ db/eventsTableCreate.js
@@ -0,0 +1,29 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "Events",
+    KeySchema: [
+        { AttributeName: "title", KeyType: "HASH" }
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "title", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+    }
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err)
+        console.log(JSON.stringify(err, null, 2));
+    else
+        console.log(JSON.stringify(data, null, 2));
+});
--- server.js
+++ server.js
@@ -2,19 +2,60 @@
 
 const
   express = require('express'),
-  bodyParser = require('body-parser')
-  // aws = require('aws')
+  bodyParser = require('body-parser'),
+  AWS = require('aws-sdk')
+
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+})
+AWS.config.credentials = new AWS.SharedIniFileCredentials()
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient()
 
 let app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(express.static('www'))
+app.use(express.static('.'))
 let router = express.Router()
 
-router.get('/events', (res, req) => {
-  res.json({message: 'not implemented'})
-});
+router.route('/events')
+  .post((req, res) => {
+    let params = {
+      TableName: 'Events',
+      Item: req.body
+    }
+
+    dynamoClient.put(params, (err, data) => {
+      if (err) res.status(500).json({message: 'error in database'})
+      else     res.status(200).json({message: 'success'})
+    })
+  })
+  .get((req, res) => {
+    let params = {TableName: 'Events'}
+    dynamoClient.scan(params, (err, data) => {
+      if (err) res.status(500).json({message: 'error in database'})
+      else     res.status(200).json(data)
+    })
+  })
+
+router.route('/events/:title')
+  .get((req, res) => {
+    let params = {
+      TableName: 'Events',
+      Key: {
+        title: req.params.title
+      }
+    }
+    dynamoClient.get(params, (err, data) => {
+      if (err) res.status(400).json({message: 'event not found'})
+      else     res.status(200).json(data)
+    });
+  })
+
+app.use('/api', router)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
