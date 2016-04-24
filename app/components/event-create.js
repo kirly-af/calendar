c183fb5757eab7aa4ed63ea0316eb7ea394f16a6
Alexandre Fulgoni <a.fulgon@gmail.com>
2016-04-24 18:30:37 +0100

    new version of event-create

--- app/components/event-create.js
+++ app/components/event-create.js
@@ -8,27 +8,90 @@ class EventCreateController
     'ngInject';
 
     this.api = API;
+    this.error = false;
+    this.step = 1;
+    this.slot = {};
+
+    // NOTE: MOCK
+    // this.title = 'test';
+    // this.description = 'decription';
+    //
+    // this.step = 2;
+    //
+    // this.duration = 10;
+    // this.datesList = [
+    //   {
+    //     id: 0,
+    //     date: '05/04/2016'
+    //   },
+    //   {
+    //     id: 1,
+    //     date: '06/04/2016'
+    //   },
+    //   {
+    //     id: 2,
+    //     date: '07/04/2016'
+    //   }
+    // ];
   }
 
-  createEvent()
+  createSlot()
   {
     let event = {
-      title: this.title,
-      description: this.description,
-      start: moment(this.start).format('DD/MM/YYYY'),
-      end: moment(this.end).format('DD/MM/YYYY'),
-      time: moment(this.start).format('HH:mm A'),
-      duration: Number(this.duration),
-      slots: Number(this.slots)
+      title: `${this.title}/${this.slot.name}`,
+      event: {
+        title: this.title,
+        description: this.description,
+        start: this.start,
+        end: this.end
+      },
+      name: this.slot.name,
+      date: this.slot.date,
+      time: moment(this.slot.time).format('HH:mm A'),
+      duration: Number(this.duration)
     };
+    this.api.createEvent(event);
+    this.nextStep();
+  }
 
-    if (moment(event.start, 'L').toDate() > moment(event.end, 'L').toDate())
-      this.dateInvalid = true;
+  generateDates()
+  {
+    if (!this.start || !this.end)
+    {
+      this.error = 'Start date and end date are required.';
+      return;
+    }
+
+    let date = moment(this.start);
+    let endDate = moment(this.end);
+    this.datesList = [];
+    for (let i = 0; !date.isAfter(endDate); ++i)
+    {
+      this.datesList.push({
+        id: i,
+        date: date.format('DD/MM/YYYY')
+      });
+      date.add(1, 'days');
+    }
+
+    if (this.datesList.length === 0)
+      this.error = 'End date should be after start date.';
     else
     {
-      this.api.createEvent(event);
-      window.location = '#/calendar';
+      this.slot.date = this.datesList[0];
+      this.nextStep();
     }
+  }
+
+  nextStep()
+  {
+    this.step += 1;
+  }
+
+  prevStep()
+  {
+    this.error = false;
+    this.step -= 1;
   }
 }
 
